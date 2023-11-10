98e96f5d423561f18bfdef8bc7f6bf784e974dd8
Erasmo Ribeiro <erasmo.ads.tech@gmail.com>
2023-11-09 22:14:35 -0300

    Configuring CORS in Spring Boot projects

---
+++ src/main/java/com/whale/web/config/WebConfiguration.java
@@ -0,0 +1,23 @@
+package com.whale.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration {
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/api/v1/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}

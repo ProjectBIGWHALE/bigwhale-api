3e9ae95745997e73a8b9fe0e5bffb2d2f7e21257
bioramonmoratori <ramon.moratori@estudante.ufjf.br>
2023-11-11 12:32:29 -0300

    Security Controller convertido para API Rest

--- src/main/java/com/whale/web/security/SecurityController.java
+++ src/main/java/com/whale/web/security/SecurityController.java
@@ -1,71 +1,62 @@
 package com.whale.web.security;
 
-import com.whale.web.security.cryptograph.model.CryptographyForm;
 import com.whale.web.security.cryptograph.service.EncryptService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Objects;
 
 
-@Controller
-@RequestMapping("/security")
+@RestController
+@RequestMapping("api/v1/security")
+@Tag(name = "API for cryptograph and decryptograph files")
 public class SecurityController {
 
-    private final CryptographyForm form;
+    @Autowired
+    private EncryptService encryptService;
 
-    private final EncryptService encryptService;
-
-    public SecurityController(CryptographyForm form, EncryptService encryptService) {
-        this.form = form;
-        this.encryptService = encryptService;
-    }
-
-    @GetMapping(value = "/cryptograph")
-    public String cryptograph(Model model) {
-
-        model.addAttribute("form", form);
-        return "cryptograph";
-    }
-
-    @PostMapping("/cryptograph")
-    public String cryptograph(CryptographyForm form, HttpServletResponse response) throws IOException {
+    @PostMapping(value = "/cryptograph", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Cryptograph Archive", description = "Convert Archive for a cryptograph or decrypted version", method = "POST")
+    public ResponseEntity<?> cryptograph(@RequestPart("file") MultipartFile file, @RequestParam("key") String key, @RequestParam("action") Boolean action) throws IOException {
 
         try {
 
             byte[] encryptedFile;
-            String originalFilename = form.getFile().getOriginalFilename();
+            String originalFilename = file.getOriginalFilename();
             String originalFileNameWithoutExtension = StringUtils.stripFilenameExtension(Objects.requireNonNull(originalFilename));
 
-            if (Boolean.TRUE.equals(form.getAction())) {
-                encryptedFile = encryptService.encryptFile(form);
-                response.setHeader("Content-Disposition", "attachment; filename=" + originalFilename + ".encrypted");
+            if (Boolean.TRUE.equals(action)) {
+                encryptedFile = encryptService.encryptFile(file, key);
+                
+                return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + originalFilename + ".encrypted")
+                    .header(CacheControl.noCache().toString())
+                    .body(encryptedFile);
             } else {
-                encryptedFile = encryptService.decryptFile(form);
-                response.setHeader("Content-Disposition", "attachment; filename=" + originalFileNameWithoutExtension);
+                encryptedFile = encryptService.decryptFile(file, key);
+            
+                return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + originalFileNameWithoutExtension)
+                    .header(CacheControl.noCache().toString())
+                    .body(encryptedFile);
             }
 
-            response.setContentType("application/octet-stream");
-            response.setContentLength(encryptedFile.length);
-            response.setHeader("Cache-Control", "no-cache");
-
-            try (OutputStream outputStream = response.getOutputStream()) {
-                outputStream.write(encryptedFile);
-                outputStream.flush();
-            } catch (Exception e) {
-                throw new RuntimeException("Error generating encrypted file", e);
-            }
         } catch (Exception e) {
-            return "redirect:/security/cryptograph";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-
-        return null;
     }
 }

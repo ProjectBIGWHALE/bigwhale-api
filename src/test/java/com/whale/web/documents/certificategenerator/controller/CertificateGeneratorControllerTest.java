aca2fe2eb75d18d8168e32603982d38baa8e22f7
Erasmo Ribeiro <erasmo.ads.tech@gmail.com>
2024-01-26 15:39:43 -0300

    test: test for CSV file validation and test all certificate templates

--- src/test/java/com/whale/web/documents/certificategenerator/controller/CertificateGeneratorControllerTest.java
+++ src/test/java/com/whale/web/documents/certificategenerator/controller/CertificateGeneratorControllerTest.java
@@ -1,7 +1,11 @@
 package com.whale.web.documents.certificategenerator.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.whale.web.documents.certificategenerator.dto.CertificateRecordDto;
 import com.whale.web.documents.certificategenerator.enums.CertificateTypeEnum;
+import com.whale.web.exceptions.domain.WhaleCheckedException;
+import com.whale.web.utils.ImageServiceUtilTest;
+import com.whale.web.utils.JsonServiceUtilTest;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -12,9 +16,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -24,45 +31,48 @@ class CertificateGeneratorControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    private static final String CSV_CONTENT = "col1,col2,col3\nvalue1,value2,value3";
+
     @Test
     void generateCertificateAndReturnStatusCode200() throws Exception {
 
-        String csvContent = "col1,col2,col3\nvalue1,value2,value3";
         MockMultipartFile csvFileDto = new MockMultipartFile(
                 "csvFileDto",
                 "worksheet.csv",
                 MediaType.TEXT_PLAIN_VALUE,
-                csvContent.getBytes());
+                CSV_CONTENT.getBytes());
 
-        CertificateRecordDto certificateRecordDto = new CertificateRecordDto(
-                CertificateTypeEnum.COURCE,
-                "ABC dos DEVS",
-                "Ronnyscley",
-                "CTO",
-                "20",
-                "2023-09-12",
-                "São Paulo",
-                1L,
-                csvFileDto
-        );
+        // Tests certificate generation with all available certificate templates
+        for (long i = 1; i < 3; i++) {
+            CertificateRecordDto certificateRecordDto = new CertificateRecordDto(
+                    CertificateTypeEnum.COURCE,
+                    "ABC dos DEVS",
+                    "Ronnyscley",
+                    "CTO",
+                    "20",
+                    "2023-09-12",
+                    "São Paulo",
+                    i,
+                    csvFileDto
+            );
+            mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/documents/certificate-generator")
+                            .flashAttr("certificateRecordDto", certificateRecordDto)
+                            .contentType(MediaType.MULTIPART_FORM_DATA))
+                    .andExpect(status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE))
+                    .andExpect(MockMvcResultMatchers.header().string("Content-Disposition", Matchers.containsString("attachment")));
 
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/documents/certificate-generator")
-                        .flashAttr("certificateRecordDto", certificateRecordDto)
-                        .contentType(MediaType.MULTIPART_FORM_DATA))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE))
-                .andExpect(MockMvcResultMatchers.header().string("Content-Disposition", Matchers.containsString("attachment")));
+        }
     }
 
     @Test
-    void returnStatusCode401WhenFormContainsEmptyorNullField_CertiificateGenerator() throws Exception {
-
-        String csvContent = "col1,col2,col3\nvalue1,value2,value3";
+    void returnStatusCode400WhenFormContainsEmptyorNullField() throws Exception {
         MockMultipartFile csvFileDto = new MockMultipartFile(
                 "csvFileDto",
                 "worksheet.csv",
                 MediaType.TEXT_PLAIN_VALUE,
-                csvContent.getBytes());
+                CSV_CONTENT.getBytes());
 
         CertificateRecordDto certificateRecordDto = new CertificateRecordDto(
                 CertificateTypeEnum.COURCE,
@@ -76,11 +86,44 @@ class CertificateGeneratorControllerTest {
                 csvFileDto
         );
 
-
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/documents/certificate-generator")
-                        .file(csvFileDto)
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/documents/certificate-generator")
                         .flashAttr("certificateRecordDto", certificateRecordDto)
                         .contentType(MediaType.MULTIPART_FORM_DATA))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        JsonNode listFieldErrors = JsonServiceUtilTest.getJsonResponse(result).get("listFieldErrors");
+        String error = JsonServiceUtilTest.getJsonResponse(result).get("error").asText();
+
+        assertEquals("Someone Fields are is blank", error);
+        assertEquals("[{\"field\":\"speakerRole\",\"message\":\"SpeakerRole field is required\\\"\"}]", listFieldErrors.toString());
     }
+
+    @Test
+    void sendDifferentCSVFileFormatAndReturnStatusCode400() throws Exception {
+        MockMultipartFile image = ImageServiceUtilTest.createTestImage("jpeg", "image");
+        CertificateRecordDto certificateRecordDto = new CertificateRecordDto(
+                CertificateTypeEnum.COURCE,
+                "ABC dos DEVS",
+                "Ronnyscley",
+                "CTO",
+                "20",
+                "2023-09-12",
+                "São Paulo",
+                1L,
+                image
+        );
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/documents/certificate-generator")
+                        .flashAttr("certificateRecordDto", certificateRecordDto)
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        JsonNode jsonMessage = JsonServiceUtilTest.getJsonResponse(result).get("message");
+        assertEquals("The file with of names cannot be different as csv.", jsonMessage.asText());
+
+    }
+
+
 }

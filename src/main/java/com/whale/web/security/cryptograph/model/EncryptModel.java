53ed830c25f62a01d33015a78ce90dfca5bb359f
Erasmo Ribeiro <erasmo.ads.tech@gmail.com>
2024-01-26 08:59:30 -0300

    refactor: Removed unused annotations

--- src/main/java/com/whale/web/security/cryptograph/model/EncryptModel.java
+++ src/main/java/com/whale/web/security/cryptograph/model/EncryptModel.java
@@ -3,10 +3,8 @@ package com.whale.web.security.cryptograph.model;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class EncryptModel {
 

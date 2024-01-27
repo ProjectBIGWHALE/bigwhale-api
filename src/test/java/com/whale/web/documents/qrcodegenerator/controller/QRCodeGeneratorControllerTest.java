5ee2f5ab5ede04520ec439373458f6b57701c8a1
Erasmo Ribeiro <erasmo.ads.tech@gmail.com>
2024-01-27 08:30:12 -0300

    test: refactor qrcode tests

--- src/test/java/com/whale/web/documents/qrcodegenerator/controller/QRCodeGeneratorControllerTest.java
+++ src/test/java/com/whale/web/documents/qrcodegenerator/controller/QRCodeGeneratorControllerTest.java
@@ -1,12 +1,15 @@
 package com.whale.web.documents.qrcodegenerator.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.whale.web.documents.compactconverter.service.CompactConverterService;
 import com.whale.web.documents.qrcodegenerator.dto.QRCodeEmailRecordDto;
 import com.whale.web.documents.qrcodegenerator.dto.QRCodeLinkRecordDto;
 import com.whale.web.documents.qrcodegenerator.dto.QRCodeWhatsappRecordDto;
 import com.whale.web.documents.zipfilecompressor.service.ZipFileCompressorService;
+import com.whale.web.utils.JsonServiceUtilTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -21,6 +24,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -31,36 +36,36 @@ class QRCodeGeneratorControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void testQRCodeGeneratorLink() throws Exception {
+    void testQRCodeGeneratorLinkWitholorsBbyName() throws Exception {
+        String[] colorByName = {"blue", "red", "green", "yellow", "orange", "pink", "cyan", "magenta", "dark_gray", "light_gray"};
+        for(String color : colorByName) {
+            var requestDto = new QRCodeLinkRecordDto("https://www.example.com", color);
+            MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/link")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(requestDto)))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().contentType(MediaType.IMAGE_PNG))
+                    .andExpect(MockMvcResultMatchers.header().string("Content-Disposition", "attachment; filename=QRCodeLink.png"))
+                    .andReturn();
 
-        var requestDto = new QRCodeLinkRecordDto(
-                "https://www.example.com",
-                "red");
-
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/link")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(requestDto)))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.IMAGE_PNG))
-                .andExpect(MockMvcResultMatchers.header().string("Content-Disposition", "attachment; filename=QRCodeLink.png"))
-                .andReturn();
-
-        byte[] responseBytes = result.getResponse().getContentAsByteArray();
-        Assertions.assertNotNull(responseBytes);
+            byte[] responseBytes = result.getResponse().getContentAsByteArray();
+            Assertions.assertNotNull(responseBytes);
+        }
     }
 
     @Test
     void testInvalidURLQRCodeGeneratorLink() throws Exception {
-
-        var requestDto = new QRCodeLinkRecordDto(
-                "URI inválida",
-                "red");
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/link")
+        var requestDto = new QRCodeLinkRecordDto("URI inválida", "#FF0000");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/link")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDto)))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andReturn();
+
+        String error = JsonServiceUtilTest.getJsonResponse(result).get("error").asText();
+        assertEquals("Someone Fields are is blank", error);
+        JsonNode listFieldErrors = JsonServiceUtilTest.getJsonResponse(result).get("listFieldErrors");
+        assertEquals("[{\"field\":\"link\",\"message\":\"The URL provided is invalid\"}]", listFieldErrors.toString());
     }
 
     @Test
@@ -69,7 +74,7 @@ class QRCodeGeneratorControllerTest {
                 "teste@gmail.com",
                 "Teste",
                 "Este é um email de Teste",
-                "blue");
+                "rgb(0, 0, 255)");
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/email")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -92,11 +97,19 @@ class QRCodeGeneratorControllerTest {
                 "Este é um email de Teste",
                 "blue");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/email")
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/email")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDto)))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andReturn();
+
+        String error = JsonServiceUtilTest.getJsonResponse(result).get("error").asText();
+        assertEquals("Someone Fields are is blank", error);
+        JsonNode listFieldErrors = JsonServiceUtilTest.getJsonResponse(result).get("listFieldErrors");
+        assertEquals(
+                "[{\"field\":\"titleEmail\",\"message\":\"Title field is required\"},"
+                        + "{\"field\":\"email\",\"message\":\"No email was provided\"}]",
+                listFieldErrors.toString());
     }
 
     @Test
@@ -104,7 +117,7 @@ class QRCodeGeneratorControllerTest {
         var requestDto = new QRCodeWhatsappRecordDto(
                 "27997512018",
                 "Mensagem de teste",
-                "green");
+                "#FF0000");
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/whatsapp")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -119,17 +132,23 @@ class QRCodeGeneratorControllerTest {
     }
 
     @Test
-    void testInvalidURLQRCodeGeneratorWhatsapp() throws Exception {
-
+    void insertInvalidColorAndReturnStatusCode200() throws Exception {
         var requestDto = new QRCodeWhatsappRecordDto(
-                "27997512018fdfd",
-                null,
-                "green");
+                "27997512018",
+                "Mensagem de teste",
+                "invalid_color");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/whatsapp")
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/documents/qrcodegenerator/whatsapp")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDto)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.IMAGE_PNG))
+                .andExpect(MockMvcResultMatchers.header().string("Content-Disposition", "attachment; filename=QRCodeWhatsapp.png"))
                 .andReturn();
+
+        byte[] responseBytes = result.getResponse().getContentAsByteArray();
+        Assertions.assertNotNull(responseBytes);
     }
+
+
 }

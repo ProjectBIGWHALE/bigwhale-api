7625608c6f62069e66b6793c2ef30a81100ac0ab
Erasmo Ribeiro <erasmo.ads.tech@gmail.com>
2024-01-27 08:25:03 -0300

    test: refactor compactconverter tests

--- src/test/java/com/whale/web/documents/compactconverter/controller/CompactConverterControllerTest.java
+++ src/test/java/com/whale/web/documents/compactconverter/controller/CompactConverterControllerTest.java
@@ -1,10 +1,13 @@
 package com.whale.web.documents.compactconverter.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.whale.web.documents.compactconverter.service.CompactConverterService;
 import com.whale.web.documents.zipfilecompressor.service.ZipFileCompressorService;
 
+import com.whale.web.utils.ImageServiceUtilTest;
+import com.whale.web.utils.JsonServiceUtilTest;
 import com.whale.web.utils.ZipServiceUtilTest;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -39,42 +43,77 @@ class CompactConverterControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private ZipFileCompressorService compressorService;
-    @MockBean
-    private CompactConverterService compactConverterService;
-
+    private final String COMPACT_CONVERTER_URI = "http://localhost:8080/api/v1/documents/compact-converter";
 
     @Test
     void testCompactConverterForOneArchive() throws Exception {
-        MockMultipartFile file = ZipServiceUtilTest.createTestZipFile();
-        String outputFormat = "tar";
+        String[] formats = {"zip", "tar", "7z", "tar.gz"};
+        for (String format : formats) {
+            MockMultipartFile archiveFile = ZipServiceUtilTest.createEmptyZipFile();
+            assert archiveFile != null;
 
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/documents/compact-converter")
-                        .file(file)
-                        .param("outputFormat", outputFormat))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.header().string("Content-Disposition", "attachment; filename=zip-test." + outputFormat))
-                .andExpect(MockMvcResultMatchers.header().string("Content-Type", "application/octet-stream"))
-                .andReturn();       
+            MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart(COMPACT_CONVERTER_URI)
+                            .file(archiveFile)
+                            .param("outputFormat", format))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn();
+        }
     }
 
     @Test
     void testCompactConverterForTwoArchives() throws Exception {
-        MockMultipartFile file1 = ZipServiceUtilTest.createTestZipFile();
-        MockMultipartFile file2 = ZipServiceUtilTest.createTestZipFile();
+        MockMultipartFile zipFile = ZipServiceUtilTest.createEmptyZipFile();
+        MockMultipartFile zipFile2 = ZipServiceUtilTest.createEmptyZipFile();
         String outputFormat = "7z";
 
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/documents/compact-converter")
-                        .file(file1)
-                        .file(file2)
+        assert zipFile != null;
+        assert zipFile2 != null;
+        mockMvc.perform(MockMvcRequestBuilders.multipart(COMPACT_CONVERTER_URI)
+                        .file(zipFile)
+                        .file(zipFile2)
                         .param("outputFormat", outputFormat))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.header().string("Content-Disposition", "attachment; filename=zip-test." + outputFormat))
-                .andExpect(MockMvcResultMatchers.header().string("Content-Type", "application/octet-stream"))
                 .andReturn();
 
     }
+
+    @Test
+    void testInvalidFformatForConversionAndReturnStatusCcode400_CompactConverterController () throws Exception {
+        MockMultipartFile zipFile = ZipServiceUtilTest.createEmptyZipFile();
+        String outputFormat = "invalid_format";
+        assert zipFile != null;
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart(COMPACT_CONVERTER_URI)
+                        .file(zipFile)
+                        .param("outputFormat", outputFormat))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn();
+
+        JsonNode jsonResponse = JsonServiceUtilTest.getJsonResponse(result).get("message");
+        assert jsonResponse.asText()
+                .equals("Invalid format for conversion. Supported formats: 'zip', 'tar.gz', '7z' or 'tar'");
+
+    }
+
+
+    @Test
+    void testImageSubmissionAndReturnStatusCode400_CompactConverterController () throws Exception {
+        MockMultipartFile image = ImageServiceUtilTest.createTestImage("png", "files");
+        String outputFormat = "tar";
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart(COMPACT_CONVERTER_URI)
+                        .file(image)
+                        .param("outputFormat", outputFormat))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn();
+
+        JsonNode jsonResponse = JsonServiceUtilTest.getJsonResponse(result).get("message");
+        assert jsonResponse.asText().equals("Upload file not a valid zip file");
+
+    }
+
+
+
 
 
 
